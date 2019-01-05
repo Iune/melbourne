2064e38b4c222907ca245bc316723397d3c8939f
Aditya Duri <aditya.duri@gmail.com>
2019-01-05 14:55:29 -0600

    Updating App error message

--- src/main/java/org/iune/melbourne/App.java
+++ src/main/java/org/iune/melbourne/App.java
@@ -7,7 +7,7 @@ import java.io.File;
 public class App {
     public static void main (String[] args) throws java.io.IOException {
         if (args.length != 1) {
-            System.err.println("Usage: java TestDetectorFile FILENAME");
+            System.err.println("Usage: java Contest FILENAME");
             System.exit(1);
         }
 

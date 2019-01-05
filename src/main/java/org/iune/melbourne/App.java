8ccb3a6059c93606a38d5530eea8f2c1da616385
Aditya Duri <aditya.duri@gmail.com>
2019-01-05 15:10:28 -0600

    Adding basic scaffolding

---
+++ src/main/java/org/iune/melbourne/App.java
@@ -0,0 +1,13 @@
+package org.iune.melbourne;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

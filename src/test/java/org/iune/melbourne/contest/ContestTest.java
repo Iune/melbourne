179fb62e053f2f7032a9354692c909fec6569ba3
Aditya Duri <aditya.duri@gmail.com>
2019-01-05 14:52:34 -0600

    No longer testing private methods

---
+++ src/test/java/org/iune/melbourne/contest/ContestTest.java
@@ -0,0 +1,13 @@
+package org.iune.melbourne.contest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ContestTest {
+
+    @Test
+    public void placeholderTest() {
+        assertTrue(true);
+    }
+}

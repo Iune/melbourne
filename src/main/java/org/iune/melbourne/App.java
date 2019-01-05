168cb059ccf2579f43678b3a626b37149d7595e1
Aditya Duri <aditya.duri@gmail.com>
2019-01-05 15:20:37 -0600

    Adding dependency on org.iune.melbourne.contest

--- src/main/java/org/iune/melbourne/App.java
+++ src/main/java/org/iune/melbourne/App.java
@@ -1,13 +1,8 @@
 package org.iune.melbourne;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }

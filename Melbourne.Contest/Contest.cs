bc8f2246df96dc29b8d71a0b7f110350f8cd19e8
Aditya Duri <aditya.duri@gmail.com>
2020-02-14 23:25:38 -0600

    Adding initial files for Contest backend

---
+++ Melbourne.Contest/Contest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Melbourne.Contest
+{
+    public class Contest
+    {
+        public List<Entry> Entries { get; }
+        public List<string> Voters { get; }
+
+        public Contest(List<Entry> entries, List<string> voters)
+        {
+            Entries = entries;
+            Voters = voters;
+        }
+
+        public Contest()
+        {
+            // TODO: Add constructor to load from Excel file
+        }
+
+        public int NumEntries
+        {
+            get => Entries.Count;
+        }
+
+        public int NumVoters
+        {
+            get => Voters.Count;
+        }
+
+        public List<Entry> ResultsAfterVoter(int voter)
+        {
+            if (voter < 0 || voter >= Voters.Count)
+            {
+                throw new ArgumentOutOfRangeException(String.Format("Voter number %d was invalid", voter));
+            }
+
+            return Entries
+                .OrderByDescending(e => e.SortingPoints[voter])
+                .ThenByDescending(e => e.DisplayPoints[voter])
+                .ThenByDescending(e => e.VoterCount[voter])
+                .ThenByDescending(e => e.PointsCountAfterVoter(12, voter))
+                .ThenByDescending(e => e.PointsCountAfterVoter(10, voter))
+                .ThenByDescending(e => e.PointsCountAfterVoter(8, voter))
+                .ThenByDescending(e => e.PointsCountAfterVoter(7, voter))
+                .ThenByDescending(e => e.PointsCountAfterVoter(6, voter))
+                .ThenByDescending(e => e.PointsCountAfterVoter(5, voter))
+                .ThenByDescending(e => e.PointsCountAfterVoter(4, voter))
+                .ThenByDescending(e => e.PointsCountAfterVoter(3, voter))
+                .ThenByDescending(e => e.PointsCountAfterVoter(2, voter))
+                .ThenByDescending(e => e.PointsCountAfterVoter(1, voter))
+                .ThenBy(e => e.Country)
+                .ThenBy(e => e.Artist)
+                .ThenBy(e => e.Song)
+                .ToList();
+        }
+
+        public List<Entry> FinalResults
+        {
+            get
+            {
+                if(Voters.Count > 0)
+                {
+                    return ResultsAfterVoter(Voters.Count - 1);
+                }
+                else
+                {
+                    return Entries;
+                }
+            }
+        }
+    }
+}
---
+++ Melbourne.Contest/Entry.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Melbourne.Contest
+{
+    public class Entry
+    {
+        public string Country { get; }
+        public string Flag { get; }
+        public string Artist { get; }
+        public string Song { get; }
+        public List<string> Votes { get; }
+
+        public Entry(string country, string flag, string artist, string song, List<string> votes)
+        {
+            Country = country;
+            Flag = flag;
+            Artist = artist;
+            Song = song;
+            Votes = votes;
+        }
+
+        public List<int> VoterCount
+        {
+            get
+            {
+                List<int> numVoters = new List<int>();
+                int count = 0;
+                foreach (string vote in Votes)
+                {
+                    if (Int32.TryParse(vote, out int points))
+                    {
+                        count++;
+                        numVoters.Add(count);
+                    }
+                }
+                return numVoters;
+            }
+        }
+
+        public List<int> DisplayPoints
+        {
+            get
+            {
+                List<int> displayPoints = new List<int>();
+                int total = 0;
+                foreach (string vote in Votes)
+                {
+                    if (Int32.TryParse(vote, out int points))
+                    {
+                        total += points;
+                        displayPoints.Add(total);
+                    }
+                }
+                return displayPoints;
+            }
+        }
+
+        public List<bool> DisqualificationStatus
+        {
+            get
+            {
+                List<bool> disqualificationStatus = new List<bool>();
+                bool isDisqualified = false;
+                foreach (string vote in Votes)
+                {
+                    if (vote.ToLower().Equals("dq") || isDisqualified)
+                    {
+                        isDisqualified = true;
+                    }
+                    disqualificationStatus.Add(isDisqualified);
+                }
+                return disqualificationStatus;
+            }
+        }
+
+        public List<int> SortingPoints
+        {
+            get
+            {
+                List<int> sortingPoints = new List<int>();
+                for (int index = 0; index < DisplayPoints.Count; index++)
+                {
+                    if (DisqualificationStatus[index])
+                    {
+                        sortingPoints.Add(-1000);
+                    }
+                    else
+                    {
+                        sortingPoints.Add(DisplayPoints[index]);
+                    }
+                }
+                return sortingPoints;
+            }
+        }
+
+        public int PointsCountAfterVoter(int points, int voter)
+        {
+            if (voter < 0 || voter >= Votes.Count)
+            {
+                throw new ArgumentOutOfRangeException(String.Format("Voter number %d was invalid", voter));
+            }
+
+            int count = 0;
+            foreach (string vote in Votes.GetRange(0, (int) voter))
+            {
+                if (Int32.TryParse(vote, out int parsedPoints))
+                {
+                    if (points == parsedPoints)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}

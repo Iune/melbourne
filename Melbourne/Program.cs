b120862238b2f53a142abb63769af3435ca7c61b
Aditya Duri <aditya.duri@gmail.com>
2025-12-26 18:25:28 -0500

    Restructing assets

--- Melbourne/Program.cs
+++ Melbourne/Program.cs
@@ -2,32 +2,32 @@
 
 using Contest;
 using Scoreboard;
+using System.IO;
 
-class Program
+static class Program
 {
     private static void Main(string[] args)
     {
         // Required for ExcelDataReader on .NET 5.0+
         System.Text.Encoding.RegisterProvider(
             System.Text.CodePagesEncodingProvider.Instance);
-
+        
         var contest = ContestLoader.GetContestFromFile(
             "/Users/aditya/Development/contests/melbourne-dotnet/resources/1988.xlsx",
             false);
         contest.PrintFinalResults();
 
+        var assetRoot = Path.Combine(AppContext.BaseDirectory, "Assets");
         var fontsConfig = new FontsConfig(
-            "/Users/aditya/Development/contests/melbourne-dotnet/python/src/main/resources/base/fonts/ZillaSlab-Regular.otf",
-            "/Users/aditya/Development/contests/melbourne-dotnet/python/src/main/resources/base/fonts/FiraSans-Regular.otf");
+            Path.Combine(assetRoot, "fonts", "ZillaSlab-Regular.otf"),
+            Path.Combine(assetRoot, "fonts", "FiraSans-Regular.otf"));
         var config = new Config(contest,
-            "/Users/aditya/Development/contests/melbourne-dotnet/python/src/main/resources/base/flags",
+            Path.Combine(assetRoot, "flags"),
             "/Users/aditya/Development/contests/melbourne-dotnet/tmp",
             "Eurovision 1988",
             fontsConfig,
             displayFlags: true);
 
         Scoreboard.Scoreboard.GenerateContestScoreboards(config);
-
-        Console.WriteLine("Hello, World!");
     }
 }

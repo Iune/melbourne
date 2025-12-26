9ba7ce2a24a58f16bceb4fa81d6540c356ac6df9
Aditya Duri <aditya.duri@gmail.com>
2025-12-26 14:16:50 -0500

    Add code to load contest from Excel

--- Melbourne/Program.cs
+++ Melbourne/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     private static void Main(string[] args)
     {
+        // Required for ExcelDataReader on .NET 5.0+
+        System.Text.Encoding.RegisterProvider(
+            System.Text.CodePagesEncodingProvider.Instance);
+
+        var contest = ContestLoader.GetContestFromFile("/Users/aditya/Development/contests/melbourne-dotnet/resources/1988.xlsx",
+            false);
+        contest.PrintFinalResults();
+        
+        
         Console.WriteLine("Hello, World!");
     }
 }
